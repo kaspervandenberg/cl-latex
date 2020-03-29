a68278d68daab6729754222ce550def4c3695e0e
stefandevai <stedevai@gmail.com>
2020-03-29 06:38:08 +0200

    Improve test readibility

--- test/integration.lisp
+++ test/integration.lisp
@@ -4,40 +4,41 @@
 
 (in-package #:cl-latex/tests)
 
-(defparameter *latex-object-result* "\\documentclass[12pt]{article}~%\\usepackage{crimson}~%\\usepackage[utf8]{inputenc}~%\\usepackage{float}~%\\usepackage{enumitem}~%\\usepackage{wrapfig}~%~%\\setlist[itemize]{label=textbullet}~%\\setlength{\\parskip}{1em}~%~%\\usepackage{hyperref}~%\\usepackage[T1]{fontenc}~%~%\\begin{document}~%~%\\section*{Menhirs en Bretagne}~%This is some text~%~%~%\\end{document}~%")
-(defparameter *latex-macro-result* "\\documentclass[12pt]{article}~%\\usepackage{crimson}~%\\usepackage[utf8]{inputenc}~%\\usepackage{float}~%\\usepackage{enumitem}~%\\usepackage{wrapfig}~%~%\\setlist[itemize]{label=textbullet}~%\\setlength{\\parskip}{1em}~%~%\\begin{document}~%~%\\section*{Menhirs en Bretagne}~%This is some text~%~%\\end{document}~%")
+(defvar *latex-object-result* "\\documentclass[12pt]{article}~%\\usepackage{crimson}~%\\usepackage[utf8]{inputenc}~%\\usepackage{float}~%\\usepackage{enumitem}~%\\usepackage{wrapfig}~%~%\\setlist[itemize]{label=textbullet}~%\\setlength{\\parskip}{1em}~%~%\\usepackage{hyperref}~%\\usepackage[T1]{fontenc}~%~%\\begin{document}~%~%\\section*{Menhirs en Bretagne}~%This is some text~%~%~%\\end{document}~%")
+(defvar *latex-macro-result* "\\documentclass[12pt]{article}~%\\usepackage{crimson}~%\\usepackage[utf8]{inputenc}~%\\usepackage{float}~%\\usepackage{enumitem}~%\\usepackage{wrapfig}~%~%\\setlist[itemize]{label=textbullet}~%\\setlength{\\parskip}{1em}~%~%\\begin{document}~%~%\\section*{Menhirs en Bretagne}~%This is some text~%~%\\end{document}~%")
 
 (deftest latex-macro-generation
   (testing "LaTeX generation with a macro"
-	(ok (equal (format nil *latex-macro-result*)
-			   (latex :documentclass (:class :article :options "12pt")
-					  :packages ((:name "crimson")
-								 (:name "inputenc" :options "utf8")
-								 (:name "float")
-								 (:name "enumitem")
-								 (:name "wrapfig"))
-					  
-					  :preamble ("\\setlist[itemize]{label=\textbullet}"
-								 (setlength "\\parskip" "1em"))
-					  
-					  :body (begin-end "document" "~%"
-									   (section* "Menhirs en Bretagne")
-									   "This is some text"
-									   "~%"))))))
+    (let ((latex-string (latex :documentclass (:class :article :options "12pt")
+                 :packages ((:name "crimson")
+                            (:name "inputenc" :options "utf8")
+                            (:name "float")
+                            (:name "enumitem")
+                            (:name "wrapfig"))                 
+                 :preamble ("\\setlist[itemize]{label=\textbullet}"
+                            (setlength "\\parskip" "1em"))                 
+                 :body (begin-end "document" "~%"
+                                  (section* "Menhirs en Bretagne")
+                                  "This is some text"
+                                  "~%"))))
+      
+      (ok (equal (format nil *latex-macro-result*)
+                 latex-string)))))
 
 (deftest latex-object-generation
   (testing "LaTeX generation with a latex-document object"
-	(ok (equal (format nil *latex-object-result*)
-			   (let ((ltx (make-latex :documentclass '(:class :article :options "12pt")
-									  :packages '((:name "crimson")
-												  (:name "inputenc" :options "utf8")
-												  (:name "float")
-												  (:name "enumitem")
-												  (:name "wrapfig"))
+    (let ((latex-document (make-latex :documentclass '(:class :article :options "12pt")
+                           :packages '((:name "crimson")
+                                       (:name "inputenc" :options "utf8")
+                                       (:name "float")
+                                       (:name "enumitem")
+                                       (:name "wrapfig"))
 
-									  :preamble (list "\\setlist[itemize]{label=\textbullet}"
-													  (setlength "\\parskip" "1em"))
-
-									  :body (list (section* "Menhirs en Bretagne")
-												  "This is some text"))))
-				 (to-string ltx))))))
+                           :preamble (list "\\setlist[itemize]{label=\textbullet}"
+                                           (setlength "\\parskip" "1em"))
+                           
+                           :body (list (section* "Menhirs en Bretagne")
+                                       "This is some text"))))
+      
+      (ok (equal (format nil *latex-object-result*)
+                 (to-string latex-document))))))
